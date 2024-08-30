806931e5aef877a19614bf1e9be061440e532467
sebastian <sebby331@gmail.com>
2024-08-30 11:40:19 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as sl
+
+with sl.container():
+    V_SPACE(7)
+    sl.markdown("<h1 style='text-align: center; color: #0078d7;'>SHOW ME THE DATA</h1>", unsafe_allow_html=True)
+    V_SPACE(7)
+    sl.write('Please  \nScroll:arrow_double_down:')
